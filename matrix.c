453998f2e3411cfc19ad158c517bd30e9f8c466d
Aaron <atrites92@ku.edu>
2026-03-02 18:12:01 -0600

    added functions and pseudocode

--- matrix.c
+++ matrix.c
@@ -1,7 +1,34 @@
 // @author Aaron Trites
 #include <stdio.h>
 
-int main(){
+int readFile(){
 
     return 0;
+}
+
+int main(){
+    //Variable Declaration
+
+
+    //Read file
+
+
+    /* GENERATE REPORT */
+    //Monthly Sales Report
+    printf("Monthly Sales Report:\n\n");
+
+
+    //Sales Summary Report
+    printf("Sales Summary Report:\n\n");
+
+
+    //Six-Month Moving Average Report
+    printf("Six-Month Moving Average Report:\n\n");
+
+
+    //Sales Report (highest to lowest)
+    printf("Sales Report (highest to lowest):\n\n");
+
+
+    return 0;  
 }
